86f757fbbbd23f006105503c4799627af9ad1e90
gmtzdev <gerardomtzisc@gmail.com>
2025-08-29 08:58:54 -0600

    ✨ feat(web): Implements a PrimeNG Theme and define css global variables with the color palette

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,23 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+        theme: {
+            preset: Aura,
+            options: {
+              darkModeSelector: '.my-app-dark'
+            }
+        }
+    }),
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi())
